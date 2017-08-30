76aae2a7ef86432efa5216b2e96a50a48f7aaa6b
Gavin Chua <me@gavinchua.com>
2017-08-30 14:42:45 +0800

    master

--- tasks/options/cssmin.js
+++ tasks/options/cssmin.js
@@ -1,12 +1,12 @@
 module.exports = {
 	pluginsCSS: {
 		files: {
-			'deploy/assets/css/plugins.min.css': ['src/css/bootstrap/bootstrap.min.css', 'src/css/plugins/*.css']
+			'deploy/assets/css/plugins.min.css': ['src/css/bootstrap/bootstrap.min.css']
 		}
 	},
 	styleCSS: {
 		files: {
-			'deploy/assets/css/style.min.css': ['src/css/structure/style.css']
+			'deploy/assets/css/style.min.css': ['src/css/structure/style.css', 'src/css/plugins/*.css']
 		}
 	}
 }
