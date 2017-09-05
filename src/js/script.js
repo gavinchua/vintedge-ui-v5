2b9462b215cecfe786852d9c9ecf3972b761bd4e
Gavin Chua <me@gavinchua.com>
2017-09-05 15:18:36 +0800

    master

--- src/js/script.js
+++ src/js/script.js
@@ -67,7 +67,7 @@ Functions
 			};
 			(function(){
 				var wf = document.createElement('script');
-				wf.src = ('https:' == document.location.protocol ? 'https' : 'http') + '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+				wf.src = ('https:' == document.location.protocol ? 'https' : 'http') + '://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js';
 				wf.type = 'text/javascript';
 				wf.async = 'true';
 				var s = document.getElementsByTagName('script')[0];
